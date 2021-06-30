a691b8630315c731f878e20b1572d097989a228c
Valentin <marian.rusoiu99@e-uvt.ro>
2021-07-01 02:57:23 +0300

    Update

--- Index.php
+++ Index.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="index.css">
+        <link rel="stylesheet" type="text/css" href="lines.css">
         <link rel="stylesheet" type="text/css" href="https://marianrusoiu99.github.io/blankAE/CSS/blankAE.css"/>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -12,21 +13,35 @@
     <body>
         <div class="screen">    
             <div class="container">
+                <div class="lines">
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                  </div>
                 <div class="text-position-right">
                     <p class="big-text">Hello,          I'm Valentin</p>
                     <p class="sub-text"> I made this website to serve as a hub for my projects and for the things I love.
                      I have many interests such an Computer Science, Web Design and design in general, Art and Philosophy.
                      I also wanted to become an arhitect:) .
                     Here are the projects I have been talking about:</p>
-
                 </div>
+                
                 <div class="text-position-left">
-                    <p class="big-text">My mind is <b>GHIVECI</b></p>
+                    <p class="big-text-ghiveci">My mind is <b>GHIVECI</b></p>
                     <p class="sub-text-oblique">GHIVÉCI, (1) ghivece, (2) ghiveciuri, s. n. 1. Vas de pământ ars, de material plastic etc., de formă tronconică, folosit pentru plantarea (în casă a) florilor. ◊ Ghiveci nutritiv = amestec de pământ, nisip, mraniță, îngrășăminte chimice etc., în care se plantează răsadurile de legume. 2. Mâncare preparată din tot felul de legume, cu sau fără carne. ♦ Fig. (Peior.) Creație literară, muzicală etc. eterogenă și lipsită de valoare. – Din tc. güvec.</p>
                 </div> 
-                
+                <div>
+                <?php 
+                echo "asdasdasdasasddceva";
+                ?>    
+                </div>
                   
             </div>
+            
         </div>
+        
     </body>
 </html>
